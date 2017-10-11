049da418786c30baafa08281df4f9cccd46519f8
SiamKing <thesiameseelectric@gmail.com>
2017-10-11 15:40:09 -0700

    created seed data for database and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = ["Luke", "Han", "Leia", "Obi", "Chewbacca", "C3P0"]
+
+meals = ["Tattooine Tacos", "Chewie's Chowder", "Princess Pizza", "Darth's Death Stack", "Brain du Jar Jar", "R2's Nuts and Bolts"]
+
+i = 0
+
+6.times do
+  UserMeal.create(user_id: User.create(name: users[i]).id, meal_id: Meal.create(title: meals[i]).id)
+  i += 1
+end
